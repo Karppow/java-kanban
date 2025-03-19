87ca462c81515fc3ba9809fed015d26a035e24a4
karppow <tesserackt@yandex.ru>
2025-03-19 20:49:31 +0300

    Поправил удаление задач и подзадач

--- src/service/InMemoryTaskManager.java
+++ src/service/InMemoryTaskManager.java
@@ -113,14 +113,18 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public void deleteTask(int id) {
-        tasks.remove(id);
-        historyManager.remove(id);
+        Task task = tasks.remove(id);
+        if (task != null) {
+            prioritizedTasks.remove(task); // Удаляем задачу из приоритетного списка
+            historyManager.remove(id);
+        }
     }
 
     @Override
     public void deleteSubtask(int id) {
         Subtask subtask = subtasks.remove(id);
         if (subtask != null) {
+            prioritizedTasks.remove(subtask); // Удаляем подзадачу из приоритетного списка
             Epic epic = epics.get(subtask.getEpicId());
             if (epic != null) {
                 epic.removeSubtask(id);
